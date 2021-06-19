e5226455a15a166b87e3c611e57fb4e94d3656f7
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-06-19 17:34:56 -0400

    Get all users

--- API-Testing/src/test/java/api/testing/ApiTests.java
+++ API-Testing/src/test/java/api/testing/ApiTests.java
@@ -9,10 +9,22 @@ public class ApiTests {
 	@Test
 	public void getReponse() {
 
+		/*
+		 * facts or Rest Assured Pattern Given : Specify prerequisites When : Describes
+		 * the action to be take Then : Describes the expected result
+		 */
 		String endpoint = "https://reqres.in/api/users";
 		var response = given().when().get(endpoint).then();
-		response.log().body();
+		// response.log().body();
 
 	}
 
+	@Test
+	public void getUserByID() {
+
+		String endpoint = "https://reqres.in/api/users";
+		var response = given().queryParam("id", 2).when().get(endpoint).then();
+
+		response.log().body();
+	}
 }
