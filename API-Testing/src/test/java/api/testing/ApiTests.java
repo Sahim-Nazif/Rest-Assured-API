11fd60ef5d3f24ce9a1b5142d471469e70f4d31f
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-06-19 19:03:04 -0400

    post and update employee info

--- API-Testing/src/test/java/api/testing/ApiTests.java
+++ API-Testing/src/test/java/api/testing/ApiTests.java
@@ -4,6 +4,8 @@ import static io.restassured.RestAssured.given;
 
 import org.junit.jupiter.api.Test;
 
+import api.user.User;
+
 public class ApiTests {
 
 	@Test
@@ -13,7 +15,7 @@ public class ApiTests {
 		 * facts or Rest Assured Pattern Given : Specify prerequisites When : Describes
 		 * the action to be take Then : Describes the expected result
 		 */
-		String endpoint = "https://reqres.in/api/users";
+		String endpoint = "http://dummy.restapiexample.com/api/v1/employees";
 		var response = given().when().get(endpoint).then();
 		// response.log().body();
 
@@ -22,39 +24,46 @@ public class ApiTests {
 	@Test
 	public void getUserByID() {
 
-		String endpoint = "https://reqres.in/api/users";
-		var response = given().queryParam("id", 2).when().get(endpoint).then();
+		String endpoint = "http://dummy.restapiexample.com/api/v1/employee";
+		var response = given().queryParam("id", 1).when().get(endpoint).then();
 
 		response.log().body();
 	}
 
 	// Posting (creating a user)
+
 	@Test
 	public void postUser() {
 
-		String endpoint = "https://reqres.in/api/users";
+		String endpoint = "http://dummy.restapiexample.com/api/v1/create";
+		User user = new User(3, "John", 42222, 38, "");
 
-		String body = "";
-		var response = given().body(body).when().post(endpoint).then();
+		var response = given().body(user).when().post(endpoint).then();
 		response.log().body();
 
 	}
 
 	// update a user
+
 	@Test
 	public void updateUser() {
-		String endpoint = "https://reqres.in/api/users";
+		String endpoint = "http://dummy.restapiexample.com/public/api/v1/update/21";
 
-		String body = "";
-		var response = given().body(body).when().put(endpoint).then();
+		User updateUser = new User();
+		updateUser.setEmployee_age(33);
+		var response = given().body(updateUser).when().put(endpoint).then();
 
 	}
 
 	// delete a user
+
 	@Test
 	public void deleteUser() {
 		String endpoint = "https://reqres.in/api/users";
 		String body = "";
+		var response = given().body(body).when().delete(endpoint).then();
+		response.log().body();
 
 	}
+
 }
